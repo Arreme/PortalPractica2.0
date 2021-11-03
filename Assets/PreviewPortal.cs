0a7b74bbd8d530bac56d342fbbacfa898110de84
arreme <a.reme@hotmail.com>
2021-11-03 15:22:43 +0100

    Portal Detection works

--- Assets/PreviewPortal.cs
+++ Assets/PreviewPortal.cs
@@ -8,25 +8,28 @@ public class PreviewPortal : MonoBehaviour
     [SerializeField] private List<Transform> _controlPoints;
     [SerializeField] private PortalGun _rayCastInfo;
 
-    [SerializeField] private float limit;
+    [SerializeField] private float _limit;
+    [SerializeField] private float _angleThreshold;
     public bool isValidPosition()
     {
-        return _controlPoints.All(x => checkPoint(x));
+        foreach (Transform point in _controlPoints)
+        {
+            if (Physics.Raycast(_rayCastInfo._playerCamera.transform.position, point.transform.position - _rayCastInfo._playerCamera.transform.position, out RaycastHit hitInfo, float.MaxValue, _rayCastInfo._portalLayerMask))
+            {
+                if (!hitInfo.transform.gameObject.CompareTag(_rayCastInfo._portalEnabledTag))
+                    return false;
+
+
+                if (Vector3.Distance(hitInfo.point, point.transform.position) > _limit)
+                    return false;
+
+
+                if (Vector3.Angle(hitInfo.normal, point.transform.forward) > _angleThreshold)
+                    return false;
+            }
+            else return false;
+        }
+        return true;
     }
 
-    private bool checkPoint(Transform point)
-    {
-        Ray r = _rayCastInfo._playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
-        if (Physics.Raycast(r, out RaycastHit hitInfo, _rayCastInfo._maxDistance + 20,_rayCastInfo._portalLayerMask))
-        {
-            Vector3 diff = point.position - hitInfo.point;
-            float distance = diff.magnitude;
-            Debug.Log(distance);
-            if (distance >= limit) return false;
-            if (!hitInfo.transform.CompareTag(_rayCastInfo._portalEnabledTag)) return false;
-            //Forward
-            return true;
-        }
-        return false;
-    }
 }
